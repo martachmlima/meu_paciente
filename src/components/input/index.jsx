9157e7b01161ba6550c0a34523cef83d16a3d26a
Filipe Melo <filipexpert63@gmail.com>
2022-01-25 12:03:39 -0300

    feat: new feature - component input

---
+++ src/components/input/index.jsx
@@ -0,0 +1,28 @@
+import { Conteiner } from "./StyleInput";
+
+const InputComponent = ({ errors, label, register, valueRegister, type }) => {
+  return (
+    <>
+      <Conteiner errors={errors}>
+        {errors ? (
+          <label>{label + " - " + errors}</label>
+        ) : (
+          <label>{label}</label>
+        )}
+
+        <input {...register(valueRegister)} type={type} />
+      </Conteiner>
+    </>
+  );
+};
+
+export default InputComponent;
+// Exemplo de como utilizar o input
+
+// <InputComponent
+
+// label={"Teste de input"}
+// errors={"errors"}
+// register={register}
+// valueRegister={"email"}
+// />
