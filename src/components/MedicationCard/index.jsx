8b64babd83c6feea852574eb1c6799f693ac9285
marta <martachmlima@gmail.com>
2022-01-27 13:46:43 -0300

    feat: add medication and patch medication functions

--- src/components/MedicationCard/index.jsx
+++ src/components/MedicationCard/index.jsx
@@ -1,11 +1,11 @@
 import { Card, CardHeader } from "./style";
 import { FaTrash } from "react-icons/fa";
 
-const MedicationCard = ({ name, frequency, time, use }) => {
+const MedicationCard = ({ name, frequency, time, use, currentFunction }) => {
   return (
     <Card>
       <CardHeader>
-        <p>Medicação: {name}</p> <FaTrash />
+        <p>Medicação: {name}</p> <FaTrash onClick={currentFunction} />
       </CardHeader>
       <p>Frequência: {frequency}</p>
       <p>Horário: {time}</p>
