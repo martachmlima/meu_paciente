a096792ec6a3e2234c4851b8524ce3d186ce49bb
Gustavo <ribeiro.gustavo95@gmail.com>
2022-01-27 20:40:32 -0300

    feat: create modal edit and add vaccines

--- src/pages/Vaccines/index.jsx
+++ src/pages/Vaccines/index.jsx
@@ -1,25 +1,41 @@
+import { useDisclosure } from "@chakra-ui/react";
 import { FaTrash } from "react-icons/fa";
+import { ModalAddVaccines } from "../../components/ModalAddVaccines";
+import { ModalEditVaccines } from "../../components/ModalEditVaccines";
 import { useVaccines } from "../../providers/VaccinesContext";
 import { Card, CardHeader } from "./style";
 
 export const Vaccines = () => {
   const { vaccines } = useVaccines();
-  console.log(vaccines);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: modalEditIsOpen,
+    onOpen: modalEditOnOpen,
+    onClose: modalEditOnClose,
+  } = useDisclosure();
   return (
     <div>
-      <button>Adicionar vacinas</button>
+      <ModalAddVaccines isOpen={isOpen} onClose={onClose} />
+      <button onClick={onOpen}>Adicionar vacinas</button>
       {vaccines.map((items) => (
-        <>
+        <div key={items.id}>
           {!items.completed && (
-            <Card>
-              <CardHeader>
-                <p>Tipo: {items.type}</p> <FaTrash />
-              </CardHeader>
-              <p>Ultima dose: {items.date}</p>
-              <p>Proxima dose: {items.nextshot}</p>
-            </Card>
+            <>
+              <Card onClick={modalEditOnOpen}>
+                <CardHeader>
+                  <p>Tipo: {items.type}</p> <FaTrash />
+                </CardHeader>
+                <p>Ultima dose: {items.date}</p>
+                <p>Proxima dose: {items.nextshot}</p>
+              </Card>
+              <ModalEditVaccines
+                isOpen={modalEditIsOpen}
+                onClose={modalEditOnClose}
+                id={items.id}
+              />
+            </>
           )}
-        </>
+        </div>
       ))}
     </div>
   );
--- src/providers/VaccinesContext.jsx
+++ src/providers/VaccinesContext.jsx
@@ -20,12 +20,43 @@ export const VaccinesProvider = ({ children }) => {
       .then((response) => setVaccines(response.data))
       .catch((err) => console.log(err));
   }, []);
-  const addVaccines = () => {
-    setVaccines([{ Hello: "oi" }]);
+  const getVaccines = (token) => {
+    if (token) {
+      api
+        .get("/vaccines", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setVaccines(response.data);
+        })
+        .catch((err) => console.log(err));
+    }
   };
-  const attVaccines = () => {};
+  const addVaccines = (data) => {
+    api
+      .post("/vaccines", data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setVaccines([...vaccines, response.data]);
+      })
+      .catch((err) => console.log(err));
+  };
+  const editVaccines = (id, data) => {
+    api
+      .patch(`/vaccines/${id}`, data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <VaccinesContext.Provider value={{ vaccines, addVaccines, attVaccines }}>
+    <VaccinesContext.Provider value={{ vaccines, addVaccines, editVaccines }}>
       {children}
     </VaccinesContext.Provider>
   );
