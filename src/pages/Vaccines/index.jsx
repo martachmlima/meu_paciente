fc869b461d3972ddd91d17ae4e65470bf8a11e3e
Gustavo <ribeiro.gustavo95@gmail.com>
2022-01-31 19:19:03 -0300

    feat: add vaccines history

--- src/pages/Vaccines/index.jsx
+++ src/pages/Vaccines/index.jsx
@@ -1,17 +1,18 @@
 import { useDisclosure } from '@chakra-ui/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ModalAddVaccines } from '../../components/ModalAddVaccines'
 import { VaccinesCard } from '../../components/VaccinesCard'
 import { useAuth } from '../../providers/AuthContext'
 import { useVaccines } from '../../providers/VaccinesContext'
-import { BoxCard, ButtonVaccine } from './style'
+import { BoxCard, ButtonVaccine, HistoricSelector } from './style'
 import Header from '../../components/Header'
 
 export const Vaccines = () => {
-  const { vaccines, getVaccines, completeVaccines } = useVaccines()
-  const { accessToken } = useAuth()
+  const { vaccines, getVaccines, completeVaccines, incompleteVaccines } =
+    useVaccines()
+  const { accessToken, logOut } = useAuth()
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const { logOut } = useAuth()
+  const [showHistoric, setShowHistoric] = useState(false)
   useEffect(() => {
     getVaccines(accessToken)
   })
@@ -21,20 +22,45 @@ export const Vaccines = () => {
 
       <ModalAddVaccines isOpen={isOpen} onClose={onClose} />
       <ButtonVaccine onClick={onOpen}>Adicionar vacinas</ButtonVaccine>
+      <HistoricSelector>
+        <p onClick={() => setShowHistoric(false)}>Ativos</p>{' '}
+        <p onClick={() => setShowHistoric(true)}>Histórico</p>
+      </HistoricSelector>
       <BoxCard>
-        {vaccines.map(items => (
-          <div key={items.id}>
-            {!items.completed && (
-              <VaccinesCard
-                type={items.type}
-                date={items.date}
-                nextshot={items.nextshot}
-                id={items.id}
-                complete={() => completeVaccines(items.id)}
-              />
-            )}
-          </div>
-        ))}
+        {showHistoric ? (
+          <>
+            {vaccines
+              .filter(item => item.completed)
+              .map(items => (
+                <div key={items.id}>
+                  <VaccinesCard
+                    type={items.type}
+                    date={items.date}
+                    nextshot={items.nextshot}
+                    id={items.id}
+                    complete={() => incompleteVaccines(items.id)}
+                    isCompleted={items.completed}
+                  />
+                </div>
+              ))}
+          </>
+        ) : (
+          <>
+            {vaccines
+              .filter(item => !item.completed)
+              .map(items => (
+                <div key={items.id}>
+                  <VaccinesCard
+                    type={items.type}
+                    date={items.date}
+                    nextshot={items.nextshot}
+                    id={items.id}
+                    complete={() => completeVaccines(items.id)}
+                  />
+                </div>
+              ))}
+          </>
+        )}
       </BoxCard>
       <ButtonVaccine onClick={logOut}>sair</ButtonVaccine>
     </div>
