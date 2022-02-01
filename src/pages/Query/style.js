f3dd233590f28553603d120fea449491fd8be7dd
Filipe Melo <filipexpert63@gmail.com>
2022-02-01 11:30:20 -0300

    style: change styled component flex appointment

--- src/pages/Query/style.js
+++ src/pages/Query/style.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const CardBox = styled.div`
   display: flex;
-  justify-content: space-between;
   width: 80vw;
   flex-wrap: wrap;
   margin: 20px auto;
