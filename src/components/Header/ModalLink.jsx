0c7b40260d6b65c02276282bb39e2ba2db075cbe
allan <allangreen2906@gmail.com>
2022-02-01 10:39:35 -0300

    feat: button logout in the header

--- src/components/Header/ModalLink.jsx
+++ src/components/Header/ModalLink.jsx
@@ -1,15 +1,23 @@
 import {
+  HStack,
+  Text,
+  Center,
   Modal,
   ModalOverlay,
   ModalHeader,
   ModalBody,
+  ModalFooter,
   ModalCloseButton,
   ModalContent,
   VStack
 } from '@chakra-ui/react'
+import { MdOutlineLogout } from 'react-icons/md'
 import LinkPage from './LinkPage'
+import { useAuth } from '../../providers/AuthContext'
 
 function ModalLink({ onClose, isOpen, actualPage }) {
+  const { logOut } = useAuth()
+
   return (
     <Modal onClose={onClose} size='full' isOpen={isOpen}>
       <ModalOverlay />
@@ -23,6 +31,22 @@ function ModalLink({ onClose, isOpen, actualPage }) {
             <LinkPage actualPage={actualPage} />
           </VStack>
         </ModalBody>
+        <ModalFooter justifyContent='flex-start' p='24px 16px'>
+          <HStack>
+            <Center
+              fontSize='50px'
+              h='60px'
+              w='50px'
+              borderRadius='8px'
+              bgColor='#e4605e'
+              color='white'
+              cursor='pointer'
+              onClick={logOut}>
+              <MdOutlineLogout />
+            </Center>
+            <Text>Sair agora</Text>
+          </HStack>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   )
